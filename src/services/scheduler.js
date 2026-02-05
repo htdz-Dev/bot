0819c53e41fbb696335d2551a077cc29472cf1c9
htdz-Dev <jsj84434@gmail.com>
2026-02-05 18:02:25 +0100

    Use custom schedule background

--- src/services/scheduler.js
+++ src/services/scheduler.js
@@ -29,7 +29,7 @@ function initScheduler(discordClient) {
 
     // Schedule countdown reminder at 18:00 daily (before Ramadan only)
     // Algiers timezone
-    eveningCountdownJob = cron.schedule('02 18 * * *', async () => {
+    eveningCountdownJob = cron.schedule('05 18 * * *', async () => {
         console.log('[Scheduler] ⏰ Countdown reminder triggered at 18:00');
         await sendCountdownOrNightOfDoubt();
     }, {
